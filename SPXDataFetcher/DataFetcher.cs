a195559dbc36a665d577e7eef4d6bc01836d8eb0
icyDenev <icy.denev@gmail.com>
2023-09-24 01:42:32 -0400

    Update source of S&P500 constituents and individual stocks

--- SPXDataFetcher/DataFetcher.cs
+++ SPXDataFetcher/DataFetcher.cs
@@ -11,33 +11,37 @@ namespace SP500DataFetcher
         static void Main(string[] args)
         {
             List<string> companiesSymbols = new List<string>();
-            DateTime dateTimeNow = DateTime.Now, dateTimePast20years = DateTime.Now.AddYears(-20);
+            Int32 unixTimestampMinus20Years = (int)DateTime.UtcNow.AddYears(-20).Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            Int32 unixTimestampNow = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            string data;
 
             // Download S&P500 companies symbols
             using (WebClient client = new WebClient())
             {
-                string url = "https://datahub.io/core/s-and-p-500-companies/r/constituents.csv";
-                string[] data = client.DownloadString(url).Split('\n');
+                string url = "https://raw.githubusercontent.com/datasets/s-and-p-500-companies/main/data/constituents.csv";
+                string[] dataSymbols = client.DownloadString(url).Split("\n");
 
                 // Extract the symbols from the data and put them into a list
-                for (int i = 1; i < data.Length; ++i)
+                for (int i = 1; i < dataSymbols.Length; ++i)
                 {
-                    string[] lineData = data[i].Split(',');
-                    companiesSymbols.Add(lineData[0]);
+                    string[] lineData = dataSymbols[i].Split(',');
+                    if (!lineData[0].Equals(""))
+                    {
+                        companiesSymbols.Add(lineData[0]);
+                    }
                 }
-            }
 
-            // Download S&P500 companies historical data
-            for (int i = 0; i < companiesSymbols.Count; i++) // TODO: Download conventions for S&P500 companies, extract the symbols, and put them into a list
-            {
-                using (WebClient client = new WebClient())
-                {
-                    string url = $"https://www.wsj.com/market-data/quotes/{companiesSymbols[i]}/historical-prices/download?MOD_VIEW=page&num_rows=5100&range_days=7395&startDate={dateTimePast20years.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)}&endDate={dateTimeNow.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)}";
-                    string data = client.DownloadString(url);
+                // Download S&P500 companies historical data
+                for (int i = 0; i < companiesSymbols.Count; i++)
+                { // TODO: Download conventions for S&P500 companies, extract the symbols, and put them into a list
+                    url = $"https://query1.finance.yahoo.com/v7/finance/download/{companiesSymbols[i]}?period1={unixTimestampMinus20Years}&period2={unixTimestampNow}&interval=1d&events=history&includeAdjustedClose=true";
+                    data = client.DownloadString(url);
                     File.WriteAllText($"{companiesSymbols[i]}.csv", data);
-                }
 
-                Console.WriteLine($"Downloaded {companiesSymbols[i]}.csv");
+                    Console.WriteLine($"Downloaded {companiesSymbols[i]}.csv");
+
+                    Thread.Sleep(200); // Sleep for 1 second to avoid Yahoo! Finance API rate limit
+                }
             }
         }
     }
